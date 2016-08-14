d6897d40682c5b42f29c52dad2ca28a9323dfeb9
William Tumeo <tumeowilliam@gmail.com>
2016-08-14 14:31:14 -0300

    Animation teste

--- scripts/color_palette.gd
+++ scripts/color_palette.gd
@@ -1,4 +1,9 @@
+extends Node
+
 export var color_1 = Color('#dff7d2')
 export var color_2 = Color('#8bbe74')
 export var color_3 = Color('#336751')
 export var color_4 = Color('#2e453b')
+
+func _ready():
+	print("Palette")
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,29 @@
+extends Node
+
 export var idle = Vector2()
-export var pressed = Vector2()
+export var tap = Vector2()
+export var hold = Vector2()
+export var hold_time = 20
+
+var press_count = 0
 
 func _ready():
 	set_process_input(true)
 	set_process(true)
 	get_child(0).set_scale(idle)
+	get_child(0).get_child(0).play("idle")
 	print(get_children())
 	print(Input.is_action_pressed("one_button"))
-
+	
 
 func _process(delta):
+	press_count += 1
 	if Input.is_action_pressed("one_button"):
-		self.get_child(0).set_scale(pressed)
+		if press_count > hold_time:
+			self.get_child(0).set_scale(hold)
+		else:
+			self.get_child(0).set_scale(tap)
 	else:
+		press_count = 0
 		self.get_child(0).set_scale(idle)
 		
