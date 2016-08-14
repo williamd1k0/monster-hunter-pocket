8c7e8cb89da10617807aa94db9082aba5344d1c8
William Tumeo <tumeowilliam@gmail.com>
2016-08-14 18:11:19 -0300

    Drake animation test

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,12 +6,14 @@ export var hold = Vector2()
 export var hold_time = 20
 
 var press_count = 0
+var sprite = null
 
 func _ready():
+	sprite = get_child(0)
 	set_process_input(true)
 	set_process(true)
-	get_child(0).set_scale(idle)
-	get_child(0).get_child(0).play("idle")
+	sprite.set_scale(idle)
+	sprite.get_child(0).play("idle")
 	print(get_children())
 	print(Input.is_action_pressed("one_button"))
 	
@@ -20,10 +22,10 @@ func _process(delta):
 	press_count += 1
 	if Input.is_action_pressed("one_button"):
 		if press_count > hold_time:
-			self.get_child(0).set_scale(hold)
+			sprite.set_scale(hold)
 		else:
-			self.get_child(0).set_scale(tap)
+			sprite.set_scale(tap)
 	else:
 		press_count = 0
-		self.get_child(0).set_scale(idle)
+		sprite.set_scale(idle)
 		
