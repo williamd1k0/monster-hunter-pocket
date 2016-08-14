c639e75f5d400a22c0462625576813e86facc9a2
William Tumeo <tumeowilliam@gmail.com>
2016-08-14 20:40:57 -0300

    Battle system wip

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,25 +7,60 @@ export var hold_time = 20
 
 var press_count = 0
 var sprite = null
+var init_press = false
+var one_tap = false
+
+var blocking = false
+var rolling = false
+var atacking = false
 
 func _ready():
 	sprite = get_child(0)
 	set_process_input(true)
 	set_process(true)
 	sprite.set_scale(idle)
-	sprite.get_child(0).play("idle")
+	show_action("Idle")
 	print(get_children())
 	print(Input.is_action_pressed("one_button"))
 	
 
 func _process(delta):
-	press_count += 1
-	if Input.is_action_pressed("one_button"):
-		if press_count > hold_time:
-			sprite.set_scale(hold)
-		else:
-			sprite.set_scale(tap)
-	else:
-		press_count = 0
-		sprite.set_scale(idle)
+	if not is_busy():
+		press_count += 1
+		if Input.is_action_pressed("one_button"):
+			if one_tap:
+				show_action("Roll")
+				reset_input()
+			else:
+				init_press = true
+				if press_count > hold_time:
+					show_action("Attack")
+					sprite.set_scale(hold)
+				
+		elif init_press:
+			if press_count < 10:
+				one_tap = true
+			init_press = false
+			
+		elif one_tap:
+			if press_count > 20:
+				show_action("Block")
+				sprite.set_scale(tap)
+				reset_input()
 		
+		else:
+			sprite.set_scale(idle)
+			reset_input()
+
+
+func is_busy():
+	return blocking or rolling or atacking
+
+func show_action(txt):
+	get_child(1).set_text(txt)
+
+
+func reset_input():
+	one_tap = false
+	init_press = false
+	press_count = 0
