0693e917e65ffca6d4973b56f14e526126d115e0
William Tumeo <tumeowilliam@gmail.com>
2016-08-16 18:04:56 -0300

    Add splash base scene

---
+++ scripts/main_splash.gd
@@ -0,0 +1,13 @@
+extends Node
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	set_process_input(true)
+
+func _input(event):
+	if event.is_action("one_button"):
+		print("Changing scene...")
+		get_tree().change_scene("res://main.tscn")
+
+
