3864d37040b54ff62fc2f511f674eb413a331f60
William Tumeo <tumeowilliam@gmail.com>
2016-08-22 02:50:32 -0300

    Fix splash

--- scripts/disclaimer.gd
+++ scripts/disclaimer.gd
@@ -3,13 +3,12 @@ extends Node
 var read = false
 
 func _ready():
-	get_child(0).get_child(0).set_opacity(0)
+	get_child(2).get_child(0).play("dotted-fadein")
 
 func _on_Timer_timeout():
 	print("Changing scene...")
 	read = true
-	get_child(0).get_child(0).get_child(0).play("fadeout")
-
+	get_child(2).get_child(0).play("dotted-fadout")
 
 func _on_AnimationPlayer_finished():
 	if read:
--- scripts/main_splash.gd
+++ scripts/main_splash.gd
@@ -46,16 +46,17 @@ func _on_AnimationPlayer_finished():
 
 func _on_Text_AnimationPlayer_finished():
 	if begin_game:
-		inst.get_child(1).show()
+		#inst.get_child(1).show()
 		inst.get_child(1).get_child(0).play("dotted-fadein")
-		inst.get_child(0).show()
-		get_child(6).start()
+		inst.get_child(0).get_child(0).play("readme")
+		print(get_node("Readme").get_wait_time())
+		get_node("Readme").start()
 
 
-func _on_Timer_2_timeout():
+func _on_Readme_timeout():
 	inst.get_child(1).get_child(0).play("dotted-fadout")
-	get_child(7).start()
+	get_node("BeforeGame").start()
 
 
-func _on_Timer_3_timeout():
+func _on_BeforeGame_timeout():
 	get_tree().change_scene("res://scenes/Main.tscn")
