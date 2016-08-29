80e4fdc72dc40a4502d1489e9d57499b9ea84de5
William Tumeo <tumeowilliam@gmail.com>
2016-08-28 22:55:25 -0300

    Minor fix

--- scripts/manager.gd
+++ scripts/manager.gd
@@ -2,10 +2,10 @@ extends Node
 
 func _ready():
 	print("Initializing!")
-	Globals.set("start_game", true)
-	if not Globals.has("max_score"):
-		Globals.set("max_score", 0)
-	Globals.save()
+	# Globals.set("start_game", true)
+	# if not Globals.has("max_score"):
+	# 	Globals.set("max_score", 0)
+	# Globals.save()
 
 func _on_Timer_timeout():
 	get_tree().change_scene("res://scenes/Disclaimer.tscn")
