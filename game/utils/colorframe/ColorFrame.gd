b830f35c8b0e6b81fcdf3b38484f7683b98bae4f
William Tumeo <tumeowilliam@gmail.com>
2017-02-08 03:09:27 -0200

    Add a simple wrapper for ColorFrame node

---
+++ game/utils/colorframe/ColorFrame.gd
@@ -0,0 +1,15 @@
+tool
+extends Control
+
+# Simple wrapper for ColorFrame node (2.2)
+
+export(Color) var color = Color(1, 1, 1)
+
+func _ready():
+	pass
+
+func draw_frame():
+	draw_rect(Rect2(get_pos(), get_size()), color)
+
+func _draw():
+	draw_frame()
