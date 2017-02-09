2c1af0415560ce56513e51cd69be37e3d2598383
William Tumeo <tumeowilliam@gmail.com>
2017-02-09 00:41:15 -0200

    Fix colorframe position bug

--- game/utils/colorframe/ColorFrame.gd
+++ game/utils/colorframe/ColorFrame.gd
@@ -9,7 +9,7 @@ func _ready():
 	pass
 
 func draw_frame():
-	draw_rect(Rect2(get_pos(), get_size()), color)
+	draw_rect(Rect2(get_node("Position2D").get_pos(), get_size()), color)
 
 func _draw():
 	draw_frame()
