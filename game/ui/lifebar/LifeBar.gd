82069276ab3115f329f0b013c2522a8b97e68aa1
William Tumeo <tumeowilliam@gmail.com>
2017-02-09 01:10:49 -0200

    Add lifebar script

---
+++ game/ui/lifebar/LifeBar.gd
@@ -0,0 +1,17 @@
+extends Control
+
+export(float) var initial_value = 100
+
+onready var life_frame = get_node("LifeFrame")
+onready var life_size = life_frame.get_size()
+onready var tween = get_node("Tween")
+
+func _ready():
+	update_bar(initial_value)
+
+func update_bar(percent):
+	tween.interpolate_method(self, '_update_bar', life_frame.get_size().x/life_size.x*100, percent, 1, 5, 0)
+	tween.start()
+
+func _update_bar(percent):
+	life_frame.set_size(Vector2(life_size.x * percent / 100, life_size.y))
